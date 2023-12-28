c2d42400c80e57d67e3e875b5aeb0c3ee2646182
Daniel Hart <me@danielhart.co>
2023-12-28 11:04:32 -0600

    Update more data

--- src/app/page.js
+++ src/app/page.js
@@ -23,24 +23,37 @@ export default function Home() {
     return splitName;
   }
 
-  const info = Object.keys(data).map((name) => (
-    <Col md={6} className='mt-4' key={name}>
-      <Card bg='secondary'>
-        <Card.Body>
-          <Card.Title>{formatNames(name)}</Card.Title>
-          <Card.Text>{getMiles(data[name].toString())}</Card.Text>
-        </Card.Body>
-      </Card>
-    </Col>
-  ));
-
   return (
     <Row>
       <Col md={12} className='mt-2'>
         <h1>Daniel Rides His Bike</h1>
         <h2>Here are some stats to keep him motivated</h2>
       </Col>
-      {info}
+      <Col md={6} className='mt-4'>
+        <Card bg='secondary'>
+          <Card.Body>
+            <Card.Title>Recent Rides</Card.Title>
+            <Card.Text>
+              <div>Total: {data.recent_ride_totals.count}</div>
+              <div>Distance: {getMiles(data.recent_ride_totals.distance)}</div>
+              <div>
+                Achivement Count: {data.recent_ride_totals.achievement_count}
+              </div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col md={6} className='mt-4'>
+        <Card bg='primary'>
+          <Card.Body>
+            <Card.Title>Total Rides</Card.Title>
+            <Card.Text>
+              <div>Total: {data.all_ride_totals.count}</div>
+              <div>Distance: {getMiles(data.all_ride_totals.distance)}</div>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </Col>
       <Row>
         <Col className='powered-by'>Powered by Strava</Col>
       </Row>
